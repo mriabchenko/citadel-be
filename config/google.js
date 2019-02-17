abbfab138c0ba7aabb43fadb53bc9bed709260cd
Nikolay_R <nikolay5011995@gmail.com>
2019-02-17 22:46:25 +0200

    google api functions

--- config/google.js
+++ config/google.js
@@ -1,5 +1,9 @@
 module.exports = {
     clientId: '864570834470-kincfl0pe90tfej15q4vugs83mcg7jg1.apps.googleusercontent.com', // e.g. asdfghjkljhgfdsghjk.apps.googleusercontent.com
     clientSecret: '9pTj1uQM5cS5VDxm_86bk06e', // e.g. _ASDFA%DFASDFASDFASD#FAD-
-    redirect: 'https://citadeli-c0c3a.firebaseapp.com/__/auth/handler' // this must match your google api settings
+    redirect: 'https://citadeli-c0c3a.firebaseapp.com/__/auth/handler', // this must match your google api settings
+    defaultScope: [
+        'https://www.googleapis.com/auth/plus.me',
+        'https://www.googleapis.com/auth/userinfo.email',
+    ]
 };
